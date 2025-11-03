b6d04661dae10a8a3a963a935fb1293ab18a668b
sxlwrl <vadymkazak2019@gmail.com>
2025-11-03 16:56:17 -0600

    change: fixed sh file

--- start.sh
+++ start.sh
@@ -1,25 +1,25 @@
-docker-compose up -d --build
+﻿docker compose up -d --build
 
 cd src/auth-service
-docker-compose up -d --build
+docker compose up -d --build
 cd ../..
 
 cd src/card-service
-docker-compose up -d --build
+docker compose up -d --build
 cd ../..
 
 cd src/history-service
-docker-compose up -d --build
+docker compose up -d --build
 cd ../..
 
 cd src/payment-service
-docker-compose up -d --build
+docker compose up -d --build
 cd ../..
 
 cd src/user-service
-docker-compose up -d --build
+docker compose up -d --build
 cd ../..
 
 cd src/verification-service
-docker-compose up -d --build
+docker compose up -d --build
 cd ../..
